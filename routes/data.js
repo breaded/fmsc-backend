a4d88a0f9348ef14d3fbf2a55a0ef9de185eaa46
Blessing Antonio <breadedx@gmail.com>
2024-08-04 21:58:00 -0300

    bug fixes and dependencies issues

--- routes/data.js
+++ routes/data.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Carrier = require('../models/Carrier');
+const Carrier = require('../models/Carriers');
 const { google } = require('googleapis');
 const credentials = require('../key.json');
 
